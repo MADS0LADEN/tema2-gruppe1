7061e0854ab587fc74fc0735ea33e7559a90a164
Mads U. Jensen <MADS0LADEN@users.noreply.github.com>
2023-11-08 14:12:43 +0100

    fix typo and send default packet of 0 to stop

--- client.py
+++ client.py
@@ -1,10 +1,10 @@
 import socket
 
-debug = True
+debug = False
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-enpoint = input("Hvilken endhed vil du snakke med?\nESP / LOCAL?\n")
-if enpoint.lower() == "esp":
+endpoint = input("Hvilken endhed vil du snakke med?\nESP / LOCAL?\n")
+if endpoint.lower() == "esp":
     server_addr = ("192.168.4.1", 7913)
 else:
     server_addr = ("127.0.0.1", 7913)
@@ -12,7 +12,7 @@ else:
 while True:
     inp = input("Send besked: ").encode()
     if not debug and len(inp) == 0:
-        inp = "NAN".encode()
+        inp = "0".encode()
     try:
         sock.sendto(inp, server_addr)
         print(server_addr, len(inp), inp.decode())
