9fea3bf861662efa03128371648305354375f32c
Mads U. Jensen <MADS0LADEN@users.noreply.github.com>
2023-11-19 19:27:46 +0100

    stuff

--- pc/keyboard.py
+++ pc/keyboard.py
@@ -16,6 +16,7 @@ dt = 0
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
+
 def send(msg):
     if len(msg) != 0:
         sock.sendto(msg.encode(), endpoint)
@@ -23,14 +24,42 @@ def send(msg):
     else:
         send("0")
 
+
 speed = 100
 trim_offset = 0
 
-def getSpeed(speed):
-    if 0 <= (speed - trim_offset) <= 100:
-        return trim_offset - speed
+
+def goStraight(speed):
+    leftSpeed = abs(speed)
+    rightSpeed = abs(speed)
+    if trim_offset < 0:
+        leftSpeed -= abs(trim_offset)
     else:
-        return speed
+        rightSpeed -= abs(trim_offset)
+    invert = "-" if speed < 0 else ""
+    send(f"{invert}{leftSpeed} {invert}{rightSpeed}")
+
+
+last_execution = 0
+
+
+def delayOffset(var):
+    global last_execution, trim_offset
+    curr_time = time.time_ns()
+    if curr_time - last_execution >= 50_000_000:
+        trim_offset = var
+        print(trim_offset)
+        last_execution = time.time_ns()
+
+
+def changeSpeed(var):
+    global last_execution, speed
+    curr_time = time.time_ns()
+    if curr_time - last_execution >= 50_000_000:
+        speed = var
+        print(speed)
+        last_execution = time.time_ns()
+
 
 while running:
     # poll for events
@@ -47,10 +76,10 @@ while running:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
         player_pos.y -= 300 * dt
-        send(f"{getSpeed(speed)} {getSpeed(speed)}")
+        goStraight(speed)
     if keys[pygame.K_s]:
         player_pos.y += 300 * dt
-        send(f"-{getSpeed(speed)} -{getSpeed(speed)}")
+        goStraight(int(f"-{speed}"))
     if keys[pygame.K_a]:
         player_pos.x -= 300 * dt
         send(f"{int(speed/2)} {int(speed/5)}")
@@ -58,6 +87,14 @@ while running:
         player_pos.x += 300 * dt
         send(f"{int(speed/5)} {int(speed/2)}")
     if keys[pygame.K_e]:
+        delayOffset(trim_offset + 1)
+    if keys[pygame.K_q]:
+        delayOffset(trim_offset - 1)
+    if keys[pygame.K_UP]:
+        changeSpeed(speed + 1)
+    if keys[pygame.K_DOWN]:
+        changeSpeed(speed - 1)
+    if keys[pygame.K_SPACE]:
         send("0")
 
     # flip() the display to put your work on screen
@@ -66,6 +103,6 @@ while running:
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
-    dt = clock.tick(60) / 1000
+    dt = clock.tick(25) / 1000
 
 pygame.quit()
