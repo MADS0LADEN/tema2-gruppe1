770b86ce0d7e31f5246cd1f02d5e8593e837acd2
Mads U. Jensen <MADS0LADEN@users.noreply.github.com>
2023-11-21 14:48:22 +0100

    delay pis, fatter intet

--- pc/keyboard.py
+++ pc/keyboard.py
@@ -3,6 +3,32 @@ import time
 
 import pygame
 
+DELAY_NS = 50_000_000
+
+
+class delay:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.delay_ns = DELAY_NS
+        self.time_ns = time.time_ns()
+
+    def __repr__(self) -> int:
+        return self.value
+
+    def set(self, value):
+        now = time.time_ns()
+        if now - self.time_ns >= self.delay_ns:
+            self = value
+            print(self.var, value)
+            self.time_ns = time.time_ns()
+
+    def get(self):
+        return self
+
+    def getDelay(self):
+        return self.delay_ns
+
+
 # socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 endpoint = ("192.168.4.1", 7913)
@@ -26,6 +52,50 @@ def send(msg):
 
 
 speed = 100
+trim_offset = 0
+turn = 10
+
+
+def goStraight(speed):
+    leftSpeed = abs(speed)
+    rightSpeed = abs(speed)
+    if trim_offset < 0:
+        leftSpeed -= abs(trim_offset)
+    else:
+        rightSpeed -= abs(trim_offset)
+    invert = "-" if speed < 0 else ""
+    send(f"{invert}{leftSpeed} {invert}{rightSpeed}")
+
+
+last_execution = 0
+
+
+def delayOffset(var):
+    global last_execution, trim_offset
+    curr_time = time.time_ns()
+    if curr_time - last_execution >= DELAY_NS:
+        trim_offset = var
+        print(trim_offset)
+        last_execution = time.time_ns()
+
+
+def changeSpeed(var):
+    global last_execution, speed
+    curr_time = time.time_ns()
+    if curr_time - last_execution >= DELAY_NS:
+        speed = var
+        print(speed)
+        last_execution = time.time_ns()
+
+
+def changeTurn(var):
+    global last_execution, turn
+    curr_time = time.time_ns()
+    if curr_time - last_execution >= DELAY_NS:
+        turn = var
+        print(turn)
+        last_execution = time.time_ns()
+
 
 while running:
     # poll for events
@@ -42,17 +112,29 @@ while running:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
         player_pos.y -= 300 * dt
-        send(f"{speed} {speed}")
+        goStraight(speed)
     if keys[pygame.K_s]:
         player_pos.y += 300 * dt
-        send(f"-{speed} -{speed}")
+        goStraight(int(f"-{speed}"))
     if keys[pygame.K_a]:
         player_pos.x -= 300 * dt
-        send(f"{int(speed/2)} {int(speed/5)}")
+        send(f"{int(speed-turn)} {int(speed)}")
     if keys[pygame.K_d]:
         player_pos.x += 300 * dt
-        send(f"{int(speed/5)} {int(speed/2)}")
+        send(f"{int(speed)} {int(speed-turn)}")
     if keys[pygame.K_e]:
+        delayOffset(trim_offset + 1)
+    if keys[pygame.K_q]:
+        delayOffset(trim_offset - 1)
+    if keys[pygame.K_UP]:
+        changeSpeed(speed + 1)
+    if keys[pygame.K_DOWN]:
+        changeSpeed(speed - 1)
+    if keys[pygame.K_LEFT]:
+        changeTurn(turn - 1)
+    if keys[pygame.K_RIGHT]:
+        changeTurn(turn + 1)
+    if keys[pygame.K_SPACE]:
         send("0")
 
     # flip() the display to put your work on screen
@@ -61,6 +143,6 @@ while running:
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
-    dt = clock.tick(60) / 1000
+    dt = clock.tick(25) / 1000
 
 pygame.quit()
